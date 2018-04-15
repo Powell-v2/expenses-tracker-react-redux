6ecaa287b378098bdb2d4b34c8e5bc54517c4ec3
Paul Ermolin <oatto.paul@gmail.com>
2018-04-15 15:28:35 +0200

    - added summary of expenses to the dashboard

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
-const ExpenseDashboardPage = () => (
+export default (props) => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
-
-export default ExpenseDashboardPage;
--- src/tests/components/ExpenseDashboardPage.test.js
+++ src/tests/components/ExpenseDashboardPage.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
 
 test("should render ExpenseDashboardPage correctly", () => {
-  const wrapper = shallow(<ExpenseDashboardPage />)
+  const wrapper = shallow(<ExpenseDashboardPage />);
+
   expect(wrapper).toMatchSnapshot();
 });
