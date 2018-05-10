0f84207cb3321442462aab626596333080230177
Paul Ermolin <oatto.paul@gmail.com>
2018-05-10 12:55:16 +0200

    - setup connection to firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "#huy#",
+  authDomain: "#huy#",
+  databaseURL: "#huy#",
+  projectId: "#huy#",
+  storageBucket: "#huy#",
+  messagingSenderId: "#huy#",
+};
+
+firebase.initializeApp(config);
